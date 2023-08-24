6e5f1264ff6d8f5280d409068022d41af44603d7
Brayan17king <xbrayansneyder@gmail.com>
2023-08-24 13:01:31 -0500

    Section-Json-Api

--- main.js
+++ main.js
@@ -60,3 +60,21 @@ let construirNav = async(path)=>{
 }
 
 construirNav("config");
+
+let construiSection1 = async(path)=>{
+    let peticion = await fetch(`${path}.json`);
+    let res = await peticion.json();
+    let seleccion = document.querySelector('#seccion1');
+    seleccion.insertAdjacentHTML('beforeend', /*html*/`
+    <div class="pricing-header py-3 pb-md text-start">
+        <h1 class="fs-6 fw-normal text-body-emphasis">${res.header.title}</h1>
+        <p class="fs-2 fw-bold">${res.header['sub-title']}</p>
+        ${res.header.parrafo.map((value) => /*html*/`
+        <p class="d-flex align-items-center fw-semibold"><svg class="bi text-danger" width="24" height="24">
+            <use xlink:href="#check"/>
+          </svg>&ensp;${value.texto}</p>`).join('')}
+      </div>
+    `)
+}
+
+construiSection1("config");
